6a41ac7188b52726af55325c01ce1a31f2608f76
jatinjatinjatin <jatinvanimina@gmail.com>
2024-09-30 14:52:39 +0530

    Nkl

--- program1.js
+++ program1.js
@@ -3,7 +3,17 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const mapping = {')':'(',']':'[','}':'{'};
+
+    for (let char of s){
+        if (mapping[char]){
+            if (stack.length === 0 || stack.pop() !== mapping[char]) return false;
+            } else {
+        stack.push(char);
+    }
+ }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -2,9 +2,29 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
+var romanToInt = function(s){
+    const romanNumerals = {
+        I:1,
+        V:5,
+        X:10,
+        L:50,
+        C:100,
+        D:500,
+        M:1000
+    };
+
+   let result = 0;
+   for(let i = 0; i<s.length;i++){
+    const currentChar = romanNumerals[s[i]];
+    const nextChar = romanNumerals[s[i+1]] || 0;
+    if(currentChar < nextChar){
+        result -= currentChar;
+   }else {
+    result += currentChar;
+   }
+}
+return result;
 };
 
 
-module.exports={romanToInt}
+module.exports={romanToInt};
